80dddd644863f916424896f1dfebb9d81ef39845
Rafael Flores <rafael.flores@ensobusiness.com>
2018-01-10 13:12:37 -0400

    React router set up with couple of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import  HomePage  from "./components/pages/HomePage";
+import  LoginPage  from "./components/pages/LoginPage";
 
 
 const App = () => (
   <div className="App">
-    <h1>React App</h1>
+    <Route path={"/"} exact component={ HomePage }/>
+    <Route path={"/login"} exact component={ LoginPage }/>
   </div>
   )
-
-
 
 export default App;
