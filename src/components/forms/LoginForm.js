b14afb3409cd369a9d27a57af08a429667c2ef07
Rafael Flores <rafael.flores@ensobusiness.com>
2018-01-11 10:38:24 -0400

    form and client side validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends Component {
     state={ 
@@ -12,13 +15,28 @@ class LoginForm extends Component {
     }
     onChange = e =>
     this.setState({
-        data: {...this.state.data, [e.target.name]: e.target.value }
+        data: { ...this.state.data, [e.target.name]: e.target.value }
     })
+
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors })
+        if (Object.keys(errors).length === 0) {
+            this.props.submit(this.state.data)
+        }
+    };
+
+    validate = data => {
+        const errors = {};
+        if (!Validator.isEmail(data.email)) errors.email = "Invalid Email";
+        if ( !data.password ) errors.password = "Can't be blank!";
+        return errors;
+    }
     render() {
-        const { data } = this.state;
+        const { data, errors } = this.state;
         return (
-            <Form>
-            <Form.Field>
+            <Form onSubmit = { this.onSubmit } >
+            <Form.Field error = {!!errors.email}>
                 <label htmlFor="email">Email</label>
                 <input 
                 name="email" 
@@ -26,8 +44,9 @@ class LoginForm extends Component {
                 onChange={ this.onChange } 
                 placeholder="example@example.com"
                 value={ data.email }/>
+            { errors.email && <InlineError text={ errors.email }/>}
             </Form.Field>
-            <Form.Field>
+            <Form.Field error = { !!errors.password} >
                 <label htmlFor="password">Password</label>
                 <input 
                 name="password" 
@@ -35,11 +54,16 @@ class LoginForm extends Component {
                 onChange={ this.onChange } 
                 placeholder="Make it secure"
                 value={ data.password }/>
+            { errors.password && <InlineError text={ errors.password }/>}
             </Form.Field>
                 <Button primary>Login</Button>
             </Form>
         );
     }
+}
+
+LoginForm.propTypes = {
+    submit: PropTypes.func.isRequired
 }
 
 export default LoginForm;
